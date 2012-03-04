3fcb81fce377171928088bd861fa0555178bfe94
Martin Fixman <martinfixman@gmail.com>
2012-03-04 15:22:08 -0500

    Something.

---
+++ android/jni/image_processing.h
@@ -0,0 +1,11 @@
+#include <opencv/cv.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+double calcCircularity(std::vector<cv::Point> contour);
+
+std::string colorSomeStuff(cv::Mat& mbgra);
+
+std::string trackRed(cv::Mat& mbgra);
